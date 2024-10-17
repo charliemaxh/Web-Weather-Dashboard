a12db9e3f130f150035f83b493166bda73cb4389
charliemaxh <cmaxholmes@gmail.com>
2024-10-17 20:52:53 +0800

    updated conf.py with url

--- docs/conf.py
+++ docs/conf.py
@@ -25,3 +25,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_baseurl = 'https://charliemaxh.github.io/Web-Weather-Dashboard/'
+
